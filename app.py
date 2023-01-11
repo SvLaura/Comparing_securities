51a2d12a2f5a52fc376e1904301b24caf77db0d0
Laura Sevostyanova <laura.sevostyanova@gmail.com>
2023-01-10 22:26:52 -0500

    changes in import (my functions)

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import numpy as np
-import invest_func
-import invest_func_wb
+from invest_func import yfin_mprices, yfin_mreturns, highlight_thebest_fin, Summary_Statistic
+from invest_func_wb import fin_data_points_wb
 
 
 symb_df = pd.read_csv('data/symbols.csv')
@@ -47,23 +47,23 @@ if (btn) and (len(option_symbol) == 3):
     lt = option_symbol
 
     # Monthly prices for the last year, 5 and 10 years
-    pricies_monthly = invest_func.yfin_mprices(lt,10)
+    pricies_monthly = yfin_mprices(lt,10)
     chart_data_prices = pricies_monthly.tail(12 * 10)
     st.header('Monthly Prices for the last 10 years')
     st.line_chart(chart_data_prices)
 
-    returns_monthly = invest_func.yfin_mreturns(lt,10)
+    returns_monthly = yfin_mreturns(lt,10)
     chart_data_returns = returns_monthly.tail(12 * 10)
     st.header('Monthly Return for the last 10 years')
     st.line_chart(chart_data_returns)
 
     st.header('Summary Statistic')
-    st.table(invest_func.Summary_Statistic(lt,10))
+    st.table(Summary_Statistic(lt,10))
 
     st.text('Please wait - calculating the main finance metrics')
     #lt = ["MSFT","AAPL","IBM"]
-    Company_Data = invest_func_wb.fin_data_points_wb(lt)
-    st.table(Company_Data.style.apply(invest_func.highlight_thebest_fin))
+    Company_Data = fin_data_points_wb(lt)
+    st.table(Company_Data.style.apply(highlight_thebest_fin))
 else:
     if btn:
         st.error('Choose only 3 companies!')
